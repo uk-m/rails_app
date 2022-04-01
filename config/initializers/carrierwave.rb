17813f70d783cbdafa3b689b6eea20f176f7b09a
yuuuuuuk <mymk426@kyf.biglobe.ne.jp>
2022-04-01 09:50:53 +0000

    s3

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -5,7 +5,7 @@ require 'carrierwave/storage/fog'
 CarrierWave.configure do |config|
   config.storage :fog
   config.fog_provider = 'fog/aws'
-  config.fog_directory  = 'S3のバケット名'
+  config.fog_directory  = 'rails-app999'
   config.fog_credentials = {
     provider: 'AWS',
     aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
