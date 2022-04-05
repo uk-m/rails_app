198355bee2511058b05871a7540e505db4154191
yuuuuuuk <mymk426@kyf.biglobe.ne.jp>
2022-04-05 10:13:56 +0000

    S3

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,12 +1,10 @@
-unless Rails.env.development? || Rails.env.test?
-  CarrierWave.configure do |config|
-    config.cache_storage = :fog
-    config.fog_directory  = 'rails-app999'
-    config.fog_credentials = {
-      provider: 'AWS',
-      aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
-      aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
-      region: 'ap-northeast-3'
-    }
-  end
+CarrierWave.configure do |config|
+  config.cache_storage = :fog
+  config.fog_directory  = 'rails-app999'
+  config.fog_credentials = {
+    provider: 'AWS',
+    aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+    aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
+    region: 'ap-northeast-1'
+  }
 end
