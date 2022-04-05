b468404fa6e9578747d63daed34acd8c14dd5817
yuuuuuuk <mymk426@kyf.biglobe.ne.jp>
2022-04-05 11:38:57 +0000

    S3

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,10 +1,16 @@
+require 'carrierwave/storage/abstract'
+require 'carrierwave/storage/file'
+require 'carrierwave/storage/fog'
+
 CarrierWave.configure do |config|
-  config.cache_storage = :fog
+  config.storage :fog
+  config.fog_provider = 'fog/aws'
   config.fog_directory  = 'rails-app999'
   config.fog_credentials = {
     provider: 'AWS',
     aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
     aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
-    region: 'ap-northeast-1'
+    region: ENV['AWS_DEFAULT_REGION'],
+    path_style: true
   }
 end
