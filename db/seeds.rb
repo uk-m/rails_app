5e1a9d4ada3a7fc56611983ec6de25e2fa4630d9
yuuuuuuk <mymk426@kyf.biglobe.ne.jp>
2022-04-09 02:20:58 +0000

    S3

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,8 +8,7 @@ User.create!(name: "Example User",
   name  = Faker::Name.name
   email = "example-#{n+1}@mail"
   password = "password"
-  User.create!(user_id: n+1,
-               name: name,
+  User.create!(name: name,
                email: email,
                password: password,
                password_confirmation: password)
@@ -23,17 +22,17 @@ following.each { |followed| user.follow(followed) }
 followers.each { |follower| follower.follow(user) }
 
 Tag.create!([
-  { tag_id: 1, name: '家電' },
-  { tag_id: 2, name: 'パソコン' },
-  { tag_id: 3, name: 'カメラ' },
-  { tag_id: 4, name: 'スマートフォン' },
-  { tag_id: 5, name: '生活' },
-  { tag_id: 6, name: 'ファッション' },
-  { tag_id: 7, name: '小物' },
-  { tag_id: 8, name: '趣味' },
-  { tag_id: 9, name: 'キッチン' },
-  { tag_id: 10, name: 'インテリア' },
-  { tag_id: 11, name: 'ゲーム' },
-  { tag_id: 12, name: 'ペット' },
-  { tag_id: 13, name: '植物' }
+  { name: '家電' },
+  { name: 'パソコン' },
+  { name: 'カメラ' },
+  { name: 'スマートフォン' },
+  { name: '生活' },
+  { name: 'ファッション' },
+  { name: '小物' },
+  { name: '趣味' },
+  { name: 'キッチン' },
+  { name: 'インテリア' },
+  { name: 'ゲーム' },
+  { name: 'ペット' },
+  { name: '植物' }
   ])
